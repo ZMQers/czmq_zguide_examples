4443a967efc4cddda3106e50ee0128515bf3d618
root <root@localhost.localdomain>
2018-02-01 16:03:48 +0000

    added version

---
+++ src/version.c
@@ -0,0 +1,46 @@
+/*  =========================================================================
+    version - description
+
+    Copyright (c) the Authors
+    =========================================================================
+*/
+
+/*
+@header
+    version -
+@discuss
+@end
+*/
+
+#include "czguide_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("version [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0;
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("version - ");
+
+    int major, minor, patch;
+    zmq_version (&major, &minor, &patch);
+    printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
+    return 0;
+}
