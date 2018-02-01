ffdeba5497fa35262ede36c88116d3be7850485f
root <root@localhost.localdomain>
2018-02-01 18:51:26 +0000

    added hwclient, hwserver

---
+++ src/wuclient.c
@@ -0,0 +1,78 @@
+/*  =========================================================================
+    wuclient - description
+
+    Copyright (c) the Authors
+    =========================================================================
+*/
+
+/*
+@header
+    wuclient -
+@discuss
+@end
+*/
+
+#include "czguide_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("wuclient [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0;
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("wuclient - Weather update client");
+
+    //  Socket to talk to server
+    printf ("Collecting updates from weather server...\n");
+    void *context = zmq_ctx_new ();
+    void *subscriber = zmq_socket (context, ZMQ_SUB);
+    int rc = zmq_connect (subscriber, "tcp://localhost:5556");
+    assert (rc == 0);
+
+    //  Subscribe to zipcode, default is NYC, 10001
+    char *filter = (argc > 1)? argv [1]: "10001 ";
+    rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE,
+                         filter, strlen (filter));
+    assert (rc == 0);
+
+    //  Process 100 updates
+    int update_nbr;
+    long total_temp = 0;
+    for (update_nbr = 0; update_nbr < 100; update_nbr++) {
+        char *string;
+        char buffer [256];
+
+        int size = zmq_recv (subscriber, buffer, 255, 0);
+        if (size != -1)
+            string = strndup (buffer, sizeof(buffer) - 1);
+
+        int zipcode, temperature, relhumidity;
+        sscanf (string, "%d %d %d",
+            &zipcode, &temperature, &relhumidity);
+        total_temp += temperature;
+        zstr_free (&string);
+    }
+    printf ("Average temperature for zipcode '%s' was %dF\n",
+        filter, (int) (total_temp / update_nbr));
+
+    zmq_close (subscriber);
+    zmq_ctx_destroy (context);
+    return 0;
+}
---
+++ src/wuserver.c
@@ -0,0 +1,65 @@
+/*  =========================================================================
+    wuserver - description
+
+    Copyright (c) the Authors
+    =========================================================================
+*/
+
+/*
+@header
+    wuserver -
+@discuss
+@end
+*/
+
+#include "czguide_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("wuserver [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0; 
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("wuserver - Weather update server");
+
+    //  Prepare our context and publisher
+    void *context = zmq_ctx_new ();
+    void *publisher = zmq_socket (context, ZMQ_PUB);
+    int rc = zmq_bind (publisher, "tcp://*:5556");
+    assert (rc == 0);
+
+    //  Initialize random number generator
+    srandom ((unsigned) time (NULL));
+    while (1) {
+        //  Get values that will fool the boss
+        int zipcode, temperature, relhumidity;
+        zipcode     = randof (100000);
+        temperature = randof (215) - 80;
+        relhumidity = randof (50) + 10;
+
+        //  Send message to all subscribers
+        char update [20];
+        sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
+        zmq_send(publisher, update, strlen(update), 0);
+    }
+    zmq_close (publisher);
+    zmq_ctx_destroy (context);
+    return 0;
+}
